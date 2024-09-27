478c4ea4814d43f4d2fc9679da5a598cc704381e
Urim Rexhepi <63608973+darewill@users.noreply.github.com>
2024-09-27 16:42:04 +0200

    installed everything needed

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar.jsx';
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import '../style/Navbar.css';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "./ui/navigation-menu";
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
+}

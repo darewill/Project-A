b7ef7c08f16194f53894a3caa70111a1d337d15c
darewill <urimm.r@gmail.com>
2024-09-28 20:28:08 +0200

    load more button add, minor style changes

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,23 +2,30 @@ import React, {useState, useEffect} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faTags, faThumbsDown, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 import '../style/Posts.css';
+import { Button } from "@/components/ui/button";
+
 export default function Posts() {
   const [posts, setPosts] = useState([]);
+  const [visible, setVisible] = useState(5);
 
   useEffect(()=>{
     const fetchPosts = async () => {
-      const response = await fetch('https://dummyjson.com/posts?limit=5');
+      const response = await fetch('https://dummyjson.com/posts');
       const data = await response.json();
       setPosts(data.posts);
     };
     fetchPosts();
   }, []);
 
+  const showMore = () => {
+    setVisible((prevValue) => prevValue + 5);
+  };
+
   return (
-    <div className='posts-wrapper flex flex-col text-center w-[700px] mx-[auto] my-[10px] text-white'>
+    <div className='posts-wrapper flex flex-col text-center w-[700px] mx-[auto] my-[10px] text-[#eeece2]'>
       <h1 className='text-3xl my-[20px]'>Posts</h1>
       <ul className=''>
-        {posts.map((post)=>(
+        {posts.slice(0,visible).map((post)=>(
           <li key={post.id} className='my-[40px] bg-[#171717] p-[40px] rounded-lg'>
             <h2 className='text-2xl font-bold mb-[20px] capitalize'>{post.title}</h2>
             <p className='font-semibold'>{post.body}</p>
@@ -32,6 +39,9 @@ export default function Posts() {
           </li>
         ))}
       </ul>
-    </div>
+      {visible < posts.length && (
+        <Button variant="secondary" className='flex m-[auto] mb-[40px] bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 h-9 px-4 py-2' onClick={showMore}>Load More</Button>
+      )}
+      </div>
   )
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,30 +1,40 @@
 import React, {useState, useEffect} from 'react';
 import '../style/Products.css';
+import { Button } from "@/components/ui/button";
+
 
 export default function Products() {
   const [products, setProducts] = useState([]);
+  const [visible, setVisible] = useState(8);
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch('https://dummyjson.com/products?limit=16');
+      const response = await fetch('https://dummyjson.com/products');
       const data = await response.json();
       setProducts(data.products);
     };
     fetchProducts();
   }, []);
+
+  const showMore = () => {
+    setVisible((prevValue) => prevValue + 8);
+  };
   
   return (
-    <div className='products-wrapper'>
-      <h1 className='text-3xl text-white mt-[30px] flex justify-center'>Products</h1>
+    <div className='products-wrapper text-[#eeece2]'>
+      <h1 className='text-3xl mt-[30px] flex justify-center'>Products</h1>
       <ul className='products-grid'>
-        {products.map((product)=>(
-          <li key={product.id} className='products-item m-[30px] flex flex-col text-center items-center bg-[#171717] text-white font-bold '>
+        {products.slice(0,visible).map((product)=>(
+          <li key={product.id} className='products-item m-[30px] flex flex-col text-center items-center bg-[#171717] font-bold '>
             <h2 className='mt-[30px] text-xl whitespace-nowrap overflow-hidden text-ellipsis max-w-[300px]'>{product.title}</h2>
             <p className='mt-[5px]'>Price: {product.price}$</p>
             <img src={product.thumbnail} alt={product.title} className='mt-[10px]'/>
           </li>
         ))}
       </ul>
-    </div>
+      {visible < products.length && (
+      <Button variant="secondary" className='flex m-[auto] mb-[40px] bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 h-9 px-4 py-2 ' onClick={showMore}>Load More</Button>
+      )}
+      </div>
   )
 }

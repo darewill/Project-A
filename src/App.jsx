1cd5203f5ac1662fa88b90f7064e564bc4204581
Urim Rexhepi <63608973+darewill@users.noreply.github.com>
2024-10-30 11:43:42 +0100

    signup page created

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar.jsx";
 import Posts from "./components/Posts";
 import Products from "./components/Products";
-import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import Login from "./components/Login";
+import Signup from "./components/Signup";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+
 
 function App() {
   const Homepage = () => {
@@ -39,6 +41,10 @@ function App() {
     {
       path: "/login",
       element: <Login />
+    },
+    {
+      path: "/signup",
+      element: <Signup />
     },
   ]);
 
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-import "../style/Login.css";
+import "../style/Signup.css";
 import { Input } from "../components/ui/input";
 import { Button } from "./ui/button";
 import { Link } from "react-router-dom";
 
-export default function Login() {
+export default function Signup() {
   return (
-    <div className='h-[600px] bg-[url("https://static.vecteezy.com/system/resources/previews/024/658/862/non_2x/3d-male-character-standing-confused-and-working-on-a-laptop-free-png.png")] bg-no-repeat bg-[length:32%] bg-[top_-2.5rem_left_12rem]'>
+    <div className='h-[600px] bg-[url("https://static.vecteezy.com/system/resources/previews/024/658/912/large_2x/3d-male-character-pointing-left-with-confidence-free-png.png")] bg-no-repeat bg-[length:32%] bg-[top_-2.5rem_left_12rem]'>
       <div className="lgn-wrapper flex justify-center flex-col items-center mt-[150px]">
         <h1 className="text-[#0e0e0e] text-[32px] font-bold mt-[80px] mb-[15px] cursor-default">
-          Login To Your Account
+          Create a New Account
         </h1>
+        <Input
+          type="firstame"
+          placeholder=" Firstname"
+          className="lgn-inp from-indigo-500 outline-none m-[10px] w-[300px] rounded "
+        />
+        <Input
+          type="lastname"
+          placeholder=" Lastname"
+          className="lgn-inp from-indigo-500 outline-none m-[10px] w-[300px] rounded "
+        />
         <Input
           type="email"
           placeholder=" Email"
@@ -25,14 +35,14 @@ export default function Login() {
           variant="secondary"
           className="lgn-btn mt-[20px] rounded w-[100px] shadow-md"
         >
-          Login
+          Signup
         </Button>
         <hr className="mt-[30px] bg-[#c3c2c2] w-[350px] h-[2px]" />
         <h1 className="text-[#0e0e0e] text-[20px] font-semibold mt-[20px] cursor-default">
-          Don't have an account?
+          Already have an account?
         </h1>
-        <Link to='/signup'>
-            <h1 className='text-[#4855b7] text-[20px] font-bold '>Register</h1>
+        <Link to='/login'>
+            <h1 className='text-[#4855b7] text-[20px] font-bold '>Login</h1>
         </Link>
       </div>
     </div>

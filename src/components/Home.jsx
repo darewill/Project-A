f48c269dd11557f2667335a991078a66aa72318e
darewill <urimm.r@gmail.com>
2024-11-11 14:04:49 +0100

    sorting added to products

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,29 +1,41 @@
-import React, {useState, useEffect} from 'react';
-import '../style/Home.css';
-import { TrendingUp } from "lucide-react"
-import { Label, Pie, PieChart } from "recharts"
-
+import React, { useState, useEffect } from "react";
+import "../style/Home.css";
+import { Link } from 'react-router-dom';
+import { Card, CardContent, CardFooter } from "../components/ui/card";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "../components/ui/card"
-import {
-  
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
-} from "../components/ui/chart"
+} from "../components/ui/chart";
+import {
+  TrendingUp,
+  Calculator,
+  Calendar,
+  CreditCard,
+  Settings,
+  Smile,
+  User,
+} from "lucide-react";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "../components/ui/command"
+
+import { Label, Pie, PieChart } from "recharts";
+
 const chartData = [
   { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
   { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
   { browser: "firefox", visitors: 287, fill: "var(--color-firefox)" },
   { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
   { browser: "other", visitors: 190, fill: "var(--color-other)" },
-]
+];
 const chartConfig = {
   visitors: {
     label: "Visitors",
@@ -48,96 +60,145 @@ const chartConfig = {
     label: "Other",
     color: "hsl(var(--chart-5))",
   },
-}
+};
 
 export default function Home() {
   const [firstname, setFirstname] = useState("");
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     const userData = localStorage.getItem("user");
-    if (userData){
+    if (userData) {
       const parsedUser = JSON.parse(userData);
       setFirstname(parsedUser.firstname);
     }
   }, []);
 
   const totalVisitors = React.useMemo(() => {
-    return chartData.reduce((acc, curr) => acc + curr.visitors, 0)
-  }, [])
+    return chartData.reduce((acc, curr) => acc + curr.visitors, 0);
+  }, []);
 
   return (
+    <div>
     <div className="home-wrapper flex">
       <div className="home-left mr-[400px]">
-      <Card className="flex flex-col">
-      <CardHeader className="items-center pb-0">
-        <CardTitle>Pie Chart - Donut with Text</CardTitle>
-        <CardDescription>January - June 2024</CardDescription>
-      </CardHeader>
-      <CardContent className="flex-1 pb-0">
-        <ChartContainer
-          config={chartConfig}
-          className="mx-auto aspect-square max-h-[250px]"
-        >
-          <PieChart>
-            <ChartTooltip
-              cursor={false}
-              content={<ChartTooltipContent hideLabel />}
-            />
-            <Pie
-              data={chartData}
-              dataKey="visitors"
-              nameKey="browser"
-              innerRadius={60}
-              strokeWidth={5}
+        <Card className="flex flex-col">
+          <CardContent className="flex-1 pb-0">
+            <ChartContainer
+              config={chartConfig}
+              className="mx-auto aspect-square max-h-[250px]"
             >
-              <Label
-                content={({ viewBox }) => {
-                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
-                    return (
-                      <text
-                        x={viewBox.cx}
-                        y={viewBox.cy}
-                        textAnchor="middle"
-                        dominantBaseline="middle"
-                      >
-                        <tspan
-                          x={viewBox.cx}
-                          y={viewBox.cy}
-                          className="fill-foreground text-3xl font-bold"
-                        >
-                          {totalVisitors.toLocaleString()}
-                        </tspan>
-                        <tspan
-                          x={viewBox.cx}
-                          y={(viewBox.cy || 0) + 24}
-                          className="fill-muted-foreground"
-                        >
-                          Visitors
-                        </tspan>
-                      </text>
-                    )
-                  }
-                }}
-              />
-            </Pie>
-          </PieChart>
-        </ChartContainer>
-      </CardContent>
-      <CardFooter className="flex-col gap-2 text-sm">
-        <div className="flex items-center gap-2 font-medium leading-none">
-          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-        </div>
-        <div className="leading-none text-muted-foreground">
-          Showing total visitors for the last 6 months
-        </div>
-      </CardFooter>
-    </Card>
+              <PieChart>
+                <ChartTooltip
+                  cursor={false}
+                  content={<ChartTooltipContent hideLabel />}
+                />
+                <Pie
+                  data={chartData}
+                  dataKey="visitors"
+                  nameKey="browser"
+                  innerRadius={60}
+                  strokeWidth={5}
+                >
+                  <Label
+                    content={({ viewBox }) => {
+                      if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                        return (
+                          <text
+                            x={viewBox.cx}
+                            y={viewBox.cy}
+                            textAnchor="middle"
+                            dominantBaseline="middle"
+                          >
+                            <tspan
+                              x={viewBox.cx}
+                              y={viewBox.cy}
+                              className="fill-foreground text-3xl font-bold"
+                            >
+                              {totalVisitors.toLocaleString()}
+                            </tspan>
+                            <tspan
+                              x={viewBox.cx}
+                              y={(viewBox.cy || 0) + 24}
+                              className="fill-muted-foreground"
+                            >
+                              Visitors
+                            </tspan>
+                          </text>
+                        );
+                      }
+                    }}
+                  />
+                </Pie>
+              </PieChart>
+            </ChartContainer>
+          </CardContent>
+          <CardFooter className="flex-col gap-2 text-sm">
+            <div className="flex items-center gap-2 font-medium leading-none">
+              Trending up by 5.2% this month{" "}
+              <TrendingUp className="h-4 w-4 text-green-500" />
+            </div>
+          </CardFooter>
+        </Card>
       </div>
       <div className="home-right text-left">
-        <h1 className='text-[30px] font-semibold  text-[#595959] mb-[10px]'>Welcome {firstname}</h1>
-        <p className="text-[25px] font-semibold  text-[#595959] w-[300px]">This is your admin panel, here you can have detailed information about your company!
+        <h1 className="text-[30px] font-semibold  text-[#595959] mb-[10px]">
+          Welcome {firstname}
+        </h1>
+        <p className="text-[25px] font-semibold  text-[#595959] w-[300px]">
+          This is your admin panel, here you can have detailed information about
+          your company!
         </p>
       </div>
     </div>
-  )
+
+    <Command className="shadow-md md:min-w-[450px] mt-[50px]">
+      <CommandInput placeholder="Type a command or search..." />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading="Suggestions">
+          <CommandItem>
+            <Calendar />
+            <span>Calendar</span>
+          </CommandItem>
+          <CommandItem>
+            <Link to='/invoice'>
+            <span>Invoices</span>
+            </Link>
+          </CommandItem>
+          <CommandItem>
+            <Link to='/products'>
+            <span>Products</span>
+            </Link>
+          </CommandItem>
+          <CommandItem>
+            <Smile />
+            <span>Search Emoji</span>
+          </CommandItem>
+          <CommandItem disabled>
+            <Calculator />
+            <span>Calculator</span>
+          </CommandItem>
+        </CommandGroup>
+        <CommandSeparator />
+        <CommandGroup heading="Settings">
+          <CommandItem>
+            <User />
+            <span>Profile</span>
+            <CommandShortcut>⌘P</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <CreditCard />
+            <span>Billing</span>
+            <CommandShortcut>⌘B</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <Settings />
+            <span>Settings</span>
+            <CommandShortcut>⌘S</CommandShortcut>
+          </CommandItem>
+        </CommandGroup>
+      </CommandList>
+    </Command>
+    </div>
+  );
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,12 +1,12 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import '../style/Products.css';
 import { Button } from "../components/ui/button";
-
 
 export default function Products() {
   const [products, setProducts] = useState([]);
   const [skip, setSkip] = useState(0);
   const limit = 8;
+  const [sortOption, setSortOption] = useState(null);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -20,23 +20,60 @@ export default function Products() {
   const loadMore = () => {
     setSkip(prevSkip => prevSkip + limit);
   };
-  
+
+  const sortProducts = () => {
+    const sortedProducts = [...products];
+    switch (sortOption) {
+      case 'lowPrice':
+        sortedProducts.sort((a, b) => a.price - b.price);
+        break;
+      case 'highPrice':
+        sortedProducts.sort((a, b) => b.price - a.price);
+        break;
+      case 'lowStock':
+        sortedProducts.sort((a, b) => a.stock - b.stock);
+        break;
+      case 'highStock':
+        sortedProducts.sort((a, b) => b.stock - a.stock);
+        break;
+      default:
+        break;
+    }
+    return sortedProducts;
+  };
+
   return (
     <div className='products-wrapper text-[#404040]'>
       <h1 className='text-3xl mt-[30px] flex justify-center font-semibold'>List of Your Current Selling Products</h1>
+
+      <select onChange={(e) => setSortOption(e.target.value)} className="sort-select mt-[40px] left">
+        <option value="">Sort Products</option>
+        <option value="lowPrice">Price: Ascending</option>
+        <option value="highPrice">Price: Descending</option>
+        <option value="lowStock">Stock: Ascending</option>
+        <option value="highStock">Stock: Descending</option>
+      </select>
+
       <ul className='products-grid'>
-        {products.map((product)=>(
-          <li key={product.id} className='products-item m-[30px] flex flex-col text-center items-center bg-white font-bold '>
+        {sortProducts().map((product) => (
+          <li key={product.id} className='products-item m-[30px] flex flex-col text-center items-center bg-white font-bold'>
             <h2 className='mt-[30px] text-xl whitespace-nowrap overflow-hidden text-ellipsis max-w-[300px]'>{product.title}</h2>
             <p className='mt-[5px]'>Price: {product.price}$</p>
             <p className='mt-[5px]'>Stock: {product.stock}</p>
             <p className='mt-[5px]'>Availability: {product.availabilityStatus}</p>
             <p className='mt-[5px]'>{product.shippingInformation}</p>
-            <img src={product.thumbnail} alt={product.title} className='mt-[10px]'/>
+            <img src={product.thumbnail} alt={product.title} className='mt-[10px]' />
           </li>
         ))}
       </ul>
-      <Button variant="secondary" className='flex m-[auto] shadow-md mb-[40px] bg-secondary text-secondary-foreground hover:bg-secondary/80 h-9 px-4 py-2 ' onClick={loadMore}>Load More</Button>
-      </div>
-  )
+
+      <Button
+        variant="secondary"
+        className='flex m-[auto] shadow-md mb-[40px] bg-secondary text-secondary-foreground hover:bg-secondary/80 h-9 px-4 py-2'
+        onClick={loadMore}
+      >
+        Load More
+      </Button>
+    </div>
+  );
 }
